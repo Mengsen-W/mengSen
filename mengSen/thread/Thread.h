571ee8723b219191636494fd04bd6c5225ead077
Mengsen-W <mengsen_wang@163.com>
2020-07-13 21:34:37 +0800

    thread

--- mengSen/thread/Thread.h
+++ mengSen/thread/Thread.h
@@ -1,28 +1,57 @@
 /*
  * @Author: Mengsen.Wang
- * @Date: 2020-07-06 21:29:09
+ * @Date: 2020-07-13 21:01:16
  * @Last Modified by: Mengsen.Wang
- * @Last Modified time: 2020-07-06 22:09:57
+ * @Last Modified time: 2020-07-13 21:24:03
  */
 
 #ifndef __MENGSEN_THREAD_H__
 #define __MENGSEN_THREAD_H__
 
-#include <functional>
+#include <pthread.h>
 
-namespace mengsen_thread {
+#include <atomic>
+#include <functional>
+#include <string>
+
+#include "CountDownLatch.h"
+
+namespace mengsen {
 
 class Thread {
  public:
   typedef std::function<void()> ThreadFunc;
+  explicit Thread(ThreadFunc, const std::string& name = std::string());
+  explicit Thread(ThreadFunc, std::string&& name = std::string());
+  ~Thread();
+
+  // non-copyable
+  Thread(const Thread& t) = delete;
+  Thread operator=(const Thread& t) = delete;
+
+  void start();
+  int join();
+
+  bool started() const { return started_; }
+  pid_t tid() const { return tid_; }
+  const std::string& name() const { return name_; }
+
+  static int32_t numCreated() { return numCreated_; }
 
  private:
   void setDefaultName();
 
+ private:
   bool started_;
   bool joined_;
+  pthread_t pthreadId_;
+  pid_t tid_;
+  ThreadFunc func_;
+  std::string name_;
+  CountDownLatch latch_;
+  static std::atomic_int32_t numCreated_;
 };
 
-}  // namespace mengsen_thread
+}  // namespace mengsen
 
 #endif  // __MENGSEN_THREAD_H__
