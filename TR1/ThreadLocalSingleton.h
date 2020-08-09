b7820f6f7e58b26670eb7ef66ac156d86d36016a
Mengsen-W <mengsen_wang@163.com>
2020-08-09 17:55:26 +0800

    TR1.2

---
+++ TR1/ThreadLocalSingleton.h
@@ -0,0 +1,69 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2020-08-09 16:22:25
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2020-08-09 17:50:07
+ */
+
+#ifndef __MENGSEN__THREADLOCALSINGLETON_H__
+#define __MENGSEN__THREADLOCALSINGLETON_H__
+
+#include <cassert>
+
+#include "noncopyable.h"
+
+namespace mengsen {
+
+template <typename T>
+class ThreadLocalSingleton : noncopyable {
+  ThreadLocalSingleton() = delete;
+  ~ThreadLocalSingleton() = delete;
+
+  static T& instance() {
+    if (!t_value) {
+      t_value = new T();
+      deleter_.set(t_value);
+    }
+    return *t_value;
+  }
+
+  static T* pointer() { return t_value; }
+
+ private:
+  static thread_local T* t_value;
+  static Deleter deleter_;
+
+ private:
+  static void destructor(void* obj) {
+    assert(obj == t_value);
+    typedef T_must_be_complate_type char[sizeof(T) == 0 ? -1 : 1];
+    T_must_be_complate_type dummy;
+    (void*)dummy;
+    delete t_value;
+    t_value = nullptr;
+  }
+
+ private:
+  class Deleter {
+   public:
+    Deleter() { pthread_key_create(&pkey_, &ThreadLocalSingleton::destructor); }
+    ~Deleter() { pthread_key_delete(pkey_); };
+
+    void set(T* newObj) {
+      assert(pthread_getspecific(pkey_) = NULL);
+      pthread_setspecific(pkey_, newObj);
+    }
+
+    pthread_key_t pkey_;
+  };
+};
+
+template <typename T>
+thread_local T* ThreadLocalSingleton<T>::t_value = nullptr;
+
+template <typename T>
+ThreadLocalSingleton<T>::Deleter ThreadLocalSingleton<T>::deleter_;
+
+}  // namespace mengsen
+
+#endif  // __MENGSEN__THREADLOCALSINGLETON_H
---
+++ test/thread/thread_race.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2020-08-09 16:12:45
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2020-08-09 16:19:42
+ */
+
+#include <unistd.h>
+
+#include <cstring>
+#include <iostream>
+#include <thread>
+
+class Test {
+ public:
+  Test() { memset(this, 0, sizeof(Test)); }
+
+ private:
+  char mem[32];
+};
+
+Test *t = nullptr;
+
+void fun() {
+  t = new Test();
+  std::cout << t << std::endl;
+  // attempting double-free in thread T2
+  sleep(1);
+  delete t;
+}
+
+int main() {
+  std::thread t1(fun);
+  std::thread t2(fun);
+
+  t1.join();
+  t2.join();
+  std::cout << t << std::endl;
+  return 0;
+}
