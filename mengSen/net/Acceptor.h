cfc04bc30d92eb7597b98bc770a5833072ffc438
mengsen <mengsen_wang@163.com>
2020-09-29 21:58:32 +0800

    Connector

---
+++ mengSen/net/Acceptor.h
@@ -0,0 +1,12 @@
+/*
+ * @Author: Mengsen.Wang 
+ * @Date: 2020-09-29 21:50:27 
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2020-09-29 21:58:03
+ */
+
+
+#ifndef __MENGSEN_NET_ACCEPTOR_H__
+#define __MENGSEN_NET_ACCEPTOR_H__
+
+#endif // __MENGSEN_NET_ACCEPTOR_H__
